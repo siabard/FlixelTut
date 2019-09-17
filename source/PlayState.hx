3ef2020338f15fc71a1d060cd4a6fdc492ef6a6a
Yeonho Jang <siabard@gmail.com>
2019-09-18 08:08:35 +0900

    적 UI 설정 / 코인 추가

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,5 +1,6 @@
 package;
 
+import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.FlxState;
 import flixel.FlxObject;
 import flixel.addons.editors.tiled.TiledMap;
@@ -13,6 +14,8 @@ class PlayState extends FlxState {
 	var _player:Player;
 	var _map:TiledMap;
 	var _mWalls:FlxTilemap;
+	var _grpCoins:FlxTypedGroup<Coin>;
+	var _grpEnemies:FlxTypedGroup<Enemy>;
 
 	override public function create():Void {
 		_map = new TiledMap(AssetPaths.room_001__tmx);
@@ -23,6 +26,13 @@ class PlayState extends FlxState {
 		_mWalls.setTileProperties(2, FlxObject.NONE);
 		_mWalls.setTileProperties(3, FlxObject.ANY);
 		add(_mWalls);
+
+		_grpCoins = new FlxTypedGroup<Coin>();
+		add(_grpCoins);
+
+		_grpEnemies = new FlxTypedGroup<Enemy>();
+		add(_grpEnemies);
+
 		_player = new Player();
 
 		var tmpMap:TiledObjectLayer = cast _map.getLayer("entities");
@@ -43,11 +53,33 @@ class PlayState extends FlxState {
 		if (entityName == "player") {
 			_player.x = x;
 			_player.y = y;
+		} else if (entityName == "coin") {
+			_grpCoins.add(new Coin(x + 4, y + 4));
+		} else if (entityName == "enemy") {
+			_grpEnemies.add(new Enemy(x + 4, y, Std.parseInt(entityData.get("etype"))));
 		}
 	}
 
 	override public function update(elapsed:Float):Void {
 		super.update(elapsed);
 		FlxG.collide(_player, _mWalls);
+		FlxG.overlap(_player, _grpCoins, playerTouchCoin);
+		FlxG.collide(_grpEnemies, _mWalls);
+		_grpEnemies.forEachAlive(checkEnemyVision);
+	}
+
+	function playerTouchCoin(P:Player, C:Coin):Void {
+		if (P.alive && P.exists && C.alive && C.exists) {
+			C.kill();
+		}
+	}
+
+	function checkEnemyVision(e:Enemy):Void {
+		if (_mWalls.ray(e.getMidpoint(), _player.getMidpoint())) {
+			e.seesPlayer = true;
+			e.playerPos.copyFrom(_player.getMidpoint());
+		} else {
+			e.seesPlayer = false;
+		}
 	}
 }
